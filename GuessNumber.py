a2963b9c138e079d82076abf631e0d5aad7e4857
Chris <65090952+chriszabo@users.noreply.github.com>
2021-02-16 22:14:36 +0100

    fixed missing colon

--- GuessNumber.py
+++ GuessNumber.py
@@ -3,7 +3,7 @@ print('Hello, what is your name?')
 name = input()
 print('Well, ' + name + ', I am thinking of a number between 1 and 20.')
 secretNumber = random.randint(1,20)
-for guessesTaken in range(1,8)
+for guessesTaken in range(1,8):
     print('Take a guess.')
     guess = int(input())
     if guess < secretNumber:
